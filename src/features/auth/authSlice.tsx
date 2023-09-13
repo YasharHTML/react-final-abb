e52cb41802f992cac4c045e3683807eee7b71a50
Yashar Ibrahimkhalilov <yashar.ibrahimkhalilov.std@bhos.edu.az>
2023-09-13 16:37:55 +0400

    IMPORTANT

--- src/features/auth/authSlice.tsx
+++ src/features/auth/authSlice.tsx
@@ -1,59 +1,100 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-  token: localStorage.getItem("token") || null,
-  error: null as string | null,
+    token: localStorage.getItem("token") || null,
+    error: null as string | null,
 };
 
 export const loginUser = createAsyncThunk(
-  "user/loginUser",
-  async (credentials: { username: string; password: string }) => {
-    try {
-      const response = await fetch(
-        "https://instagram.brightly-shining.cloud/api/v1/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(credentials),
+    "user/loginUser",
+    async (credentials: { username: string; password: string }) => {
+        try {
+            const response = await fetch(
+                "https://instagram.brightly-shining.cloud/api/v1/auth/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(credentials),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Error");
+            }
+
+            const responseData: any = await response.json();
+            if (responseData && responseData.token) {
+                const token = responseData.token;
+                return token;
+            }
+        } catch (error) {
+            throw error;
         }
-      );
-
-      if (!response.ok) {
-        throw new Error("Error");
-      }
-
-      const responseData: any = await response.json();
-      if (responseData && responseData.token) {
-        const token = responseData.token;
-        return token;
-      }
-    } catch (error) {
-      throw error;
     }
-  }
+);
+
+export const registerUser = createAsyncThunk(
+    "user/registerUser",
+    async (credentials: {
+        username: string;
+        password: string;
+        firstName: string;
+        lastName: string;
+    }) => {
+        try {
+            const response = await fetch(
+                "https://instagram.brightly-shining.cloud/api/v1/auth/register",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(credentials),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Error");
+            }
+
+            const responseData: any = await response.json();
+            if (responseData && responseData.token) {
+                const token = responseData.token;
+                return token;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 );
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    logout: (state) => {
-      state.token = null;
-      localStorage.removeItem("token");
+    name: "auth",
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.token = null;
+            localStorage.removeItem("token");
+        },
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(loginUser.fulfilled, (state, action) => {
-        state.token = action.payload;
-        localStorage.setItem("token", action.payload);
-      })
-      .addCase(loginUser.rejected, (state, action) => {
-        state.error = action.error.message ?? null;
-      });
-  },
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.token = action.payload;
+                localStorage.setItem("token", action.payload);
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.error = action.error.message ?? null;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.token = action.payload;
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.error = action.error.message ?? null;
+            });
+    },
 });
 
 export const { logout } = authSlice.actions;
